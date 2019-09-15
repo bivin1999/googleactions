5e57e7cd25f72445341f269ec6a1e60465ce4a36
bivin varkey varghese <bivinvvpacoeg@gmail.com>
2019-09-15 17:46:25 +0530

    uploaded site

---
+++ JS/googjs.js
@@ -0,0 +1,17 @@
+var countDownDate = new Date("Oct 13, 2019 09:00:00").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("D").innerHTML = days;
+  document.getElementById("H").innerHTML = hours;
+  document.getElementById("M").innerHTML = minutes;
+  document.getElementById("S").innerHTML = seconds;
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "BEGAN";
+  }
+}, 1000);
